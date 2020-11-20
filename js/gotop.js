147f69cb13660f04c8ccf9d9c860ca9a256781e8
dlawlsdud0419 <dlawlsdud0419@gmail.com>
2020-11-21 02:49:26 +0900

    scroll-modi

--- js/gotop.js
+++ js/gotop.js
@@ -14,7 +14,7 @@ function scrollIt() {
         var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
         if (currentScroll > 0) {
             window.requestAnimationFrame(smoothscroll);
-            window.scrollTo(0, currentScroll - (currentScroll / 50));
+            window.scrollTo(0, currentScroll - (currentScroll / 20));
         }
     })();
 }
