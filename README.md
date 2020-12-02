c791fba14a4c0e7c74f6741b879f0feb122cff58
dlawlsdud0419 <dlawlsdud0419@gmail.com>
2020-12-03 01:10:12 +0900

    creature

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 # redstone-information
 
 <a href="https://dlawlsdud0419.github.io/redstone/">실행ㄱㄱ</a>
-<div>붉은보석 자료모음</div>
+
+<strong>붉은보석 자료모음</strong>
--- js/gotop.js
+++ js/gotop.js
@@ -14,7 +14,7 @@ function scrollIt() {
         var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
         if (currentScroll > 0) {
             window.requestAnimationFrame(smoothscroll);
-            window.scrollTo(0, currentScroll - (currentScroll / 20));
+            window.scrollTo(0, currentScroll - (currentScroll / 10));
         }
     })();
 }
