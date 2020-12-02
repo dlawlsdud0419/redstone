9f9534bbdb25aba6242e4b3b3b52d4adc453aed3
dlawlsdud0419 <59009654+dlawlsdud0419@users.noreply.github.com>
2020-12-02 13:27:58 +0900

    Delete fileautodown.js

--- js/fileautodown.js
+++
@@ -1,8 +0,0 @@
-downloadTextFile(text, name) {
-    const a = document.createElement('a');
-    const type = name.split(".").pop();
-    a.href = URL.createObjectURL( new Blob([text], { type:`text/${type === "txt" ? "plain" : type}` }) );
-    a.download = name;
-    a.click();
-  }
-  this.downloadTextFile(JSON.stringify(gltf), 'myObj.json');
